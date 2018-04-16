a6effecf6d3f790fd6722ad2705f04e1ba8f210b
kaishibooo <>
2018-04-16 17:06:47 +0900

    環境構築　010　Spring Boot 初期セットアップ 設定ファイル（properties／logback）切り替え

--- src/main/java/prac/PracApplication.java
+++ src/main/java/prac/PracApplication.java
@@ -1,5 +1,7 @@
 package prac;
 
+import java.text.MessageFormat;
+
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -8,11 +10,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
+import org.thymeleaf.util.StringUtils;
 
 @SpringBootApplication
 public class PracApplication {
 
 	public static void main(String[] args) {
+
+		String springProfilesActive = System.getProperty("spring.profiles.active");
+		if(!StringUtils.equals(springProfilesActive, "product")
+				&& !StringUtils.equals(springProfilesActive, "develop")){
+			throw new UnsupportedOperationException(
+				MessageFormat.format(
+				"JVMの起動時引数 -Dspring.profiles.active で develop か product を指定して下さい ( -Dspring.profiles.active={0} )。"
+						, springProfilesActive));
+		}
+
 		SpringApplication.run(PracApplication.class, args);
 	}
 
